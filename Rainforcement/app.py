93cdbffb00775675782ca590fc1d3f93bd341017
Dev <jmdevnath.c@gmail.com>
2025-08-08 23:59:57 +0530

    Add libraries

--- Rainforcement/app.py
+++ Rainforcement/app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Load the imported model
 model = joblib.load('model.joblib')
