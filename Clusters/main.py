6744f06dbeea1a5baed3209c2ea47923cab804c3
Dev <jmdevnath.c@gmail.com>
2025-08-08 19:35:04 +0530

    Add Model

--- Clusters/main.py
+++ Clusters/main.py
@@ -14,3 +14,22 @@ with open('scaler.pickle', 'rb') as file:
 st.title('Customer Segmentation')
 
 st.markdown('This app segments customers based on their purchasing behavior.')
+
+age = st.slider('Age', 18, 100, 30)
+income = st.slider('Income', 1000, 100000, 50000)
+spending = st.slider('Spending Score', 1, 100, 50)
+st.write('Input values:', age, income, spending)
+
+# Make prediction
+input_data = [[age, income, spending]]
+input_scaled = scaler.transform(input_data)
+cluster = model.predict(input_scaled)[0]
+st.write('Predicted cluster:', cluster)
+
+cluster_label = {
+    0:"budget",
+    1:"premium",
+    2:"Moderate"
+}
+
+st.subheader(cluster_label[cluster])
