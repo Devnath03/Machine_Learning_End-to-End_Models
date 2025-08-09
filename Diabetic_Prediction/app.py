30d22ab33436407e2bd1ec1e56660196215f476d
Dev <jmdevnath.c@gmail.com>
2025-08-09 23:17:06 +0530

    import models

--- Diabetic_Prediction/app.py
+++ Diabetic_Prediction/app.py
@@ -4,9 +4,20 @@ import joblib
 
 # Initialize Flask app
 app = Flask(__name__)
-@app.route('/', methods=['GET'])
+
+model = joblib.load('model.pickle')
+scaler = joblib.load('scaler.pickle')
+
+@app.route('/test', methods=['POST'])
 def home():
-    return jsonify(message="Welcome to the Diabetic Prediction App")
+    try:
+      if request.is_json:
+        data = request.get_json()
+        features = scaler.transform([data['features']])
+        prediction = model.predict(features)
+        return jsonify(prediction=prediction.tolist())
+    except Exception as e:
+        return jsonify(error=str(e)), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
