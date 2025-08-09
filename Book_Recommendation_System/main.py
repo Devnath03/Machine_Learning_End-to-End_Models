0b319d205e60d11037ef16d9f2101b1e2eef5c26
Dev <jmdevnath.c@gmail.com>
2025-08-09 19:40:34 +0530

    text_feature

--- Book_Recommendation_System/main.py
+++ Book_Recommendation_System/main.py
@@ -7,7 +7,7 @@ from scipy.sparse import hstack, csc_matrix
 
 @st.cache_data()
 def load_model():
-    df = pd.read_pickle("Book_Recommend_Dataset.pkl")
+    df = pd.read_pickle("Book_Recommend_Dataset.pickle")
 
 #Save the Model
     model = pickle.load(open('knn_model.pickle', 'rb'))
@@ -18,3 +18,18 @@ def load_model():
 #Call the Function
 df, model, tfidf, scaler = load_model()
 
+book_title =  df['title'].values
+title_to_idx = {title: idx for idx, title in enumerate (book_title)}
+
+st.title("Book Recommendation System")
+
+book_title = st.selectbox("Select a book", book_title)
+
+if book_title:
+    idx =  title_to_idx[book_title]
+
+text_feature = df.iloc[idx]['text']
+
+tfidf_feature  = tfidf.transform([text_feature])
+year = scaler.transform([[df.iloc[idx]['year']]])
+combined = hstack([tfidf_feature, csc_matrix(year)])
