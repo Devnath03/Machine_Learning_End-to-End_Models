c38a155b8d627d87870f64ee5cd1b47e220b9775
Dev <jmdevnath.c@gmail.com>
2025-08-09 23:05:10 +0530

    load the server API

--- Diabetic_Prediction/app.py
+++ Diabetic_Prediction/app.py
@@ -1,10 +1,10 @@
 #import libraries
-from flask import Flask
+from flask import Flask,request, jsonify
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return "Welcome to the Diabetic Prediction App"
+    return jsonify(message="Welcome to the Diabetic Prediction App")
 
 if __name__ == '__main__':
     app.run(debug=True)
